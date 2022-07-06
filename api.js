1b1b6aa08f8b7da31da8fc0a052339a10d213116
tesorerol <luistesorero5@gmail.com>
2022-07-06 11:59:03 -0500

    fix

--- api.js
+++ api.js
@@ -33,7 +33,7 @@ app.use(cors(corsOptions))
 server.listen(PORT, () => console.log('server On ' + PORT));
 
 mongoose.connect(`mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DB}`, { useNewUrlParser: true, useUnifiedTopology: true }).then((db) => console.log("conectado")).catch((e) => console.log(e));
-
+getUpdateRows();
 setInterval(() => {
     getUpdateRows();
     console.log("Update")
